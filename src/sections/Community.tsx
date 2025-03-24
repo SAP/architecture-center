8031fe90744e913234f37704f0552145b7c61314
Pierre-Olivier BASSEVILLE <pierre-olivier.basseville@sap.com>
2025-03-24 22:48:12 +0100

    Update to stay as close as possible to the original design

--- src/sections/Community.tsx
+++ src/sections/Community.tsx
@@ -4,35 +4,35 @@ import Link from '@docusaurus/Link';
 export default function CommunitySection(): JSX.Element {
     return (
         <section>
+            <br /> <br />
             <div className="container">
                 <div className="community">
                     <div className="community_image">
-                        <img src="/img/landingPage/community.jpg" className="community_image_inside" />
+                        <img src="/img/landingPage/community_puzzle.jpg" className="community_image_inside" />
                     </div>
                     <div className="community_body">
                         <h2>
                             <b>
-                                <HighlightText>Let's Build Together</HighlightText>
+                                <HighlightText>Let's Build Together!</HighlightText>
                             </b>
                         </h2>
                         <p>
-                            Are you passionate about solution architecture and eager to share your knowledge and
-                            expertise with others? <br /> <br />
-                            The Architecture Center is the perfect place for you! Whether you're an experienced
-                            architect or someone just starting out, our community welcomes everyone who wants to
-                            contribute and collaborate on creating and sharing reference architectures. <br /> <br />
-                            This is more than just a platform; it's a collective effort to advance the field of
-                            architecture through shared knowledge and collaboration. Together, we can create a rich
-                            repository of reference architectures that benefit everyone. <br /> <br />
+                            Are you passionate about solution architecture and eager to share your knowledge and expertise with others?
+                            <br /> <br />
+                            The <b>Architecture Center</b> is the perfect place for you! Whether you're an experienced architect or someone just starting out, our community welcomes everyone who wants to contribute and collaborate on creating and sharing reference architectures.
+                            <br /> <br />
+                            This is more than just a platform; it's a collective effort to advance the field of architecture through shared knowledge and collaboration. Together, we can create a rich repository of reference architectures that benefit everyone.
+                            <br /> <br />
                         </p>
                         <div className="community_buttons">
                             <Link className="button_filled_community" to="/community/intro">
-                                Connect with Community
+                                Let's go!
                             </Link>
                         </div>
                     </div>
                 </div>
             </div>
+            <br /> <br />
         </section>
     );
 }
