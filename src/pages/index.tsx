aa3b0f716b567722ac0cb5686c36d62f6baa75fe
James Rapp <james.rapp@sap.com>
2025-05-19 15:51:37 -0600

    update page title and metadata for Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,15 +42,15 @@ export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext();
     return (
         <Layout
-            title={siteConfig.title}
+            title='Create reference architectures'
             description={siteConfig.tagline}
             metadata={[
-                { property: 'og:title', content: siteConfig.title },
+                { property: 'og:title', content: 'Create reference architectures' },
                 { property: 'og:description', content: siteConfig.tagline },
                 { property: 'og:type', content: 'website' },
                 { property: 'og:url', content: 'https://architecture.learning.sap.com/' },
                 { name: 'twitter:card', content: 'summary_large_image' },
-                { name: 'twitter:title', content: siteConfig.title },
+                { name: 'twitter:title', content: 'Create reference architectures' },
                 { name: 'twitter:description', content: siteConfig.tagline },
             ]}
         >
