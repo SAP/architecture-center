e9d65d70d999fc038f3eb7600caa67f2038c9d42
Pierre-Olivier BASSEVILLE <pierre-olivier.basseville@sap.com>
2025-08-01 08:44:55 +0200

    Changing title of the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,15 +10,15 @@ export default function Home(): JSX.Element {
     return (
         <Layout
             // @ts-expect-error
-            title="Create reference architectures"
+            title="Welcome"
             description={siteConfig.tagline}
             metadata={[
-                { property: 'og:title', content: 'Create reference architectures' },
+                { property: 'og:title', content: 'Welcome' },
                 { property: 'og:description', content: siteConfig.tagline },
                 { property: 'og:type', content: 'website' },
                 { property: 'og:url', content: 'https://architecture.learning.sap.com/' },
                 { name: 'twitter:card', content: 'summary_large_image' },
-                { name: 'twitter:title', content: 'Create reference architectures' },
+                { name: 'twitter:title', content: 'Welcome' },
                 { name: 'twitter:description', content: siteConfig.tagline },
             ]}
         >
