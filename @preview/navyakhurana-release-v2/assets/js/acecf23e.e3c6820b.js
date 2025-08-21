ac2f24b927baf27c6f4fcf5d78e810a5e55c1e5e
Navya Khurana <64686397+navyakhurana@users.noreply.github.com>
2025-08-21 11:31:34 +0530

    Delete @preview/navyakhurana-release-v2 directory

--- @preview/navyakhurana-release-v2/assets/js/acecf23e.e3c6820b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/navyakhurana-release-v2/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/navyakhurana-release-v2/blog/authors"}')}}]);
