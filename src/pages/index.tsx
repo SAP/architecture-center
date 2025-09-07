35712be6f8ba3b91c8099b207260d695b17d1047
navyakhurana <navyakhurana2000@gmail.com>
2025-09-07 18:21:27 +0530

    Test Joule

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import React, { JSX } from 'react';
+import React, { JSX, useEffect } from 'react';
 import HeroSection from '../sections/HeroSection';
 import DomainSection from '../sections/TechnologyDomainSection';
 import AdditionalResSection from '../sections/AdditionalResSection';
 
 export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext();
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://wup3nky7.eu12.sapdas-staging.cloud.sap/resources/public/webclient/bootstrap.js'; // Replace with actual URL
+        script.setAttribute('data-bot-name', 'sap_digital_assistant'); // Replace with your bot name
+        script.setAttribute('data-expander-type', 'DEFAULT');
+        script.setAttribute(
+            'data-expander-preferences',
+            'JTdCJTIyb25ib2FyZGluZ01lc3NhZ2UlMjIlM0ElMjJDb21lJTIwc3BlYWslMjB0byUyMG1lJTIxJTIyJTJDJTIyZXhwYW5kZXJUaXRsZSUyMiUzQSUyMiUyMiUyQyUyMmV4cGFuZGVyTG9nbyUyMiUzQSUyMkNVWF9BdmF0YXIuc3ZnJTIyJTJDJTIydGhlbWUlMjIlM0ElMjJzYXBfaG9yaXpvbiUyMiU3RA=='
+        );
+        document.body.appendChild(script);
+    }, []);
     return (
         <Layout
             // @ts-expect-error
