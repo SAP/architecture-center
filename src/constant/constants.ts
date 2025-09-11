2011408e5359345e9359a4d2f69d8e347ef66333
navyakhurana <navyakhurana2000@gmail.com>
2025-09-11 11:26:58 +0530

    Updated name to Quick Start

--- src/constant/constants.ts
+++ src/constant/constants.ts
@@ -3,7 +3,7 @@ export const navigationCardsData = [
     { title: 'Architecture Validator', icon: 'sap-icon://order-status', link: '' },
     { title: 'Community of Practice', icon: 'sap-icon://group', link: '/community/intro' },
     { title: 'Solution Diagram Guidelines', icon: 'sap-icon://learning-assistant', link: '' },
-    { title: 'Contribute Now', icon: 'sap-icon://write-new-document', link: '/quickStart' },
+    { title: 'Quick Start', icon: 'sap-icon://write-new-document', link: '/quickStart' },
     { title: "What's new", icon: 'sap-icon://marketing-campaign', link: '/blog' },
 ];
 
