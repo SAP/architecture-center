720da208216082d30ce7d680b93661d54d08a6ff
2096GIT_USER <2096GIT_USER_EMAIL>
2025-09-11 16:50:19 +0000

    pr build ➝ gh pages

---
+++ @preview/chbk10-feature/update-readme/assets/js/acecf23e.f65c1480.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/chbk10-feature/update-readme/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/chbk10-feature/update-readme/blog/authors"}')}}]);
