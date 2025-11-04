3905308ba469a4193e6b7f93e488a1dc84881270
paasapcom <paa@sap.com>
2025-11-04 15:51:37 +0000

    cleanup: removed folder @preview/AjitKP91-dev after merge

--- @preview/AjitKP91-dev/assets/js/acecf23e.2281abed.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/AjitKP91-dev/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/AjitKP91-dev/blog/authors"}')}}]);
