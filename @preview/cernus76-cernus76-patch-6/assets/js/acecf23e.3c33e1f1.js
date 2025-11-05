0ce3384b3bfd5b6606850220a6cdf1304f001394
paasapcom <paa@sap.com>
2025-11-05 10:15:19 +0000

    pr build ➝ gh pages

---
+++ @preview/cernus76-cernus76-patch-6/assets/js/acecf23e.3c33e1f1.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/cernus76-cernus76-patch-6/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/cernus76-cernus76-patch-6/blog/authors"}')}}]);
