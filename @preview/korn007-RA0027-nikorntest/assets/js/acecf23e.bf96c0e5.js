42e78c02513fb56a70958f219d11ce31d2e3dda9
paasapcom <paa@sap.com>
2025-11-10 03:34:30 +0000

    pr build ➝ gh pages

---
+++ @preview/korn007-RA0027-nikorntest/assets/js/acecf23e.bf96c0e5.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/korn007-RA0027-nikorntest/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/korn007-RA0027-nikorntest/blog/authors"}')}}]);
