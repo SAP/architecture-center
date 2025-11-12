0d2711b53ac767fbf19260d91354c3269af9ad73
paasapcom <paa@sap.com>
2025-11-12 14:31:47 +0000

    pr build ➝ gh pages

---
+++ @preview/johannagonnzdz-clean-up-code-from-cards/assets/js/acecf23e.d21fc8c1.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/johannagonnzdz-clean-up-code-from-cards/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/johannagonnzdz-clean-up-code-from-cards/blog/authors"}')}}]);
