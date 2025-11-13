a5ead2ecdb32e995293b33b2248f57d1d11e6ca5
paasapcom <paa@sap.com>
2025-11-13 09:34:17 +0000

    cleanup: removed folder @preview/gantysandhu-RA0027-genaibtprefad after merge

--- @preview/gantysandhu-RA0027-genaibtprefad/assets/js/acecf23e.876e8fdf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/gantysandhu-RA0027-genaibtprefad/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/gantysandhu-RA0027-genaibtprefad/blog/authors"}')}}]);
