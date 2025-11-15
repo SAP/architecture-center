c004748c504879cce10d1cd52ea8e59211ab912c
paasapcom <paa@sap.com>
2025-11-15 07:25:49 +0000

    pr build ➝ gh pages

---
+++ @preview/navyakhurana-validator-dev/assets/js/acecf23e.31dfc233.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:a=>{a.exports=JSON.parse('{"blogBasePath":"/@preview/navyakhurana-validator-dev/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/navyakhurana-validator-dev/blog/authors"}')}}]);
