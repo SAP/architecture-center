683a2699ad89c5e97ebb11711e50928116e5075b
paasapcom <paa@sap.com>
2025-11-15 08:25:17 +0000

    site build ➝ gh pages [skip ci]

---
+++ @preview/validator-dev/assets/js/acecf23e.d20c1f56.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/validator-dev/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/validator-dev/blog/authors"}')}}]);
