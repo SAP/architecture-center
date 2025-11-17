f35c8d357ef8e03f16c0c856f0f23586d17e0a0f
paasapcom <paa@sap.com>
2025-11-17 13:40:03 +0000

    cleanup: removed folder @preview/nasseritani-RA0027-sac-cpi after merge

--- @preview/nasseritani-RA0027-sac-cpi/assets/js/acecf23e.f7e72e06.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/nasseritani-RA0027-sac-cpi/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/nasseritani-RA0027-sac-cpi/blog/authors"}')}}]);
