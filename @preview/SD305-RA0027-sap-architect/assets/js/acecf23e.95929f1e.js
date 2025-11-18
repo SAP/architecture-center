2a4b1909a485a702cfdf4a077c0317f115fc1cd9
paasapcom <paa@sap.com>
2025-11-18 15:07:18 +0000

    cleanup: removed folder @preview/SD305-RA0027-sap-architect after merge

--- @preview/SD305-RA0027-sap-architect/assets/js/acecf23e.95929f1e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/SD305-RA0027-sap-architect/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/SD305-RA0027-sap-architect/blog/authors"}')}}]);
