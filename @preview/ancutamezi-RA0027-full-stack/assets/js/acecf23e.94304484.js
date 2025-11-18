8174bb6315817d6c7e8220c65f51604861bbd9db
paasapcom <paa@sap.com>
2025-11-18 15:05:02 +0000

    cleanup: removed folder @preview/ancutamezi-RA0027-full-stack after merge

--- @preview/ancutamezi-RA0027-full-stack/assets/js/acecf23e.94304484.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/ancutamezi-RA0027-full-stack/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/ancutamezi-RA0027-full-stack/blog/authors"}')}}]);
