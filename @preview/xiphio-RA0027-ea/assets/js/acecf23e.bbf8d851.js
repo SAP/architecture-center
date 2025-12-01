fbe4cc78ac64fc4e7860882f6d049a766bafa05f
paasapcom <paa@sap.com>
2025-12-01 10:52:41 +0000

    pr build ➝ gh pages

---
+++ @preview/xiphio-RA0027-ea/assets/js/acecf23e.bbf8d851.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/xiphio-RA0027-ea/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/xiphio-RA0027-ea/blog/authors"}')}}]);
