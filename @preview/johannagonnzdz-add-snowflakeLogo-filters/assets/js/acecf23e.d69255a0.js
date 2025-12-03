d2bdd5793aefb29fa75229fa594fa8e44f18c788
paasapcom <paa@sap.com>
2025-12-03 09:36:44 +0000

    cleanup: removed folder @preview/johannagonnzdz-add-snowflakeLogo-filters after merge

--- @preview/johannagonnzdz-add-snowflakeLogo-filters/assets/js/acecf23e.d69255a0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/johannagonnzdz-add-snowflakeLogo-filters/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/johannagonnzdz-add-snowflakeLogo-filters/blog/authors"}')}}]);
