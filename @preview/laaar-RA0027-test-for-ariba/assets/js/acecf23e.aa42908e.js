a1d7ae17945ea346dbe8bb19e36c1d9dd08edbd9
paasapcom <paa@sap.com>
2025-12-03 18:52:36 +0000

    cleanup: removed folder @preview/laaar-RA0027-test-for-ariba after merge

--- @preview/laaar-RA0027-test-for-ariba/assets/js/acecf23e.aa42908e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/laaar-RA0027-test-for-ariba/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/laaar-RA0027-test-for-ariba/blog/authors"}')}}]);
