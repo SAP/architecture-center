15d30874a9a23f926d9f57375516819bc5cd266e
Julian Schambeck <julian.schambeck@sap.com>
2025-12-19 11:57:53 +0100

    simplify to require only one step

--- docs/ref-arch/RA0005/3-retrieval-augmented-generation/terraform/step1/provider.tf
+++ docs/ref-arch/RA0005/3-retrieval-augmented-generation/terraform/step1/provider.tf
@@ -1,16 +1,19 @@
-# ------------------------------------------------------------------------------------------------------
-# Define the required providers for this module
-# ------------------------------------------------------------------------------------------------------
 terraform {
   required_providers {
     btp = {
-      source  = "sap/btp"
-      version = "~> 1.8.0"
+      source  = "SAP/btp"
+      version = "1.18.1"
+    }
+    cloudfoundry = {
+      source  = "cloudfoundry/cloudfoundry"
+      version = "1.11.0"
     }
   }
 }
 
 provider "btp" {
-  globalaccount  = var.globalaccount
-  cli_server_url = var.cli_server_url
+  globalaccount  = var.globalaccount_subdomain
+  cli_server_url = "https://cli.btp.cloud.sap"
 }
+
+provider "cloudfoundry" {}
---
+++ docs/ref-arch/RA0005/3-retrieval-augmented-generation/terraform/step1/terraform.tfvars
@@ -0,0 +1,8 @@
+globalaccount_subdomain = ""
+subaccount_name         = "Build on Reference Architecture"
+region                  = "eu10"
+cf_landscape_label      = "cf-eu10-004"
+cf_org_name             = "build-on-reference-architecture"
+subaccount_admins       = ["another.user@test.com", "pierre-olivier.basseville@sap.com"]
+cf_org_admins           = ["another.user@test.com", "pierre-olivier.basseville@sap.com"]
+cf_space_managers       = ["another.user@test.com", "pierre-olivier.basseville@sap.com"]
