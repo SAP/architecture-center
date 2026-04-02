9f413689bb5e4b50db0660cd1de6d425364d8716
Pierre-Olivier BASSEVILLE <pierre-olivier.basseville@sap.com>
2026-04-02 13:15:57 +0200

    update description for ai golden path

--- src/constant/constants.ts
+++ src/constant/constants.ts
@@ -9,7 +9,7 @@ export const navigationCardsData = [
     },
     {
         title: 'AI Golden Path',
-        subtitle: 'The SAP AI Golden Path is the starting point for developing AI applications across the SAP ecosystem. It contains recommendations, best practices, and tutorials to help you understand the AI technology stack, identify suitable tools and services, and design, deliver, and extend enterprise-grade AI solutions on SAP technology.\n\nIt serves as a central entry point into our approach for building AI-native applications, covering everything from data and AI foundation services to agentic architectures and Joule.',
+        subtitle: 'The SAP AI Golden Path is the starting point for developing AI applications across the SAP ecosystem. It contains recommendations, best practices, and tutorials to help you understand the AI technology stack, identify suitable tools and services, and design, deliver, and extend enterprise-grade AI solutions on SAP technology.',
         icon: 'sap-icon://initiative',
         link: '/docs/aigp/',
         isNew: true,
